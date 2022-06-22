9cb4fe388852e783c319032c754b31319c7eafc0
stebolt <steve@stebolt.ch>
2022-06-22 21:31:08 +0200

    commit boring stuff changes

--- idiot.py
+++ idiot.py
@@ -5,5 +5,5 @@ while True:
     prompt = 'Want to know how to keep an idiot busy for hours?\n'
     response = pyip.inputYesNo(prompt)
     if response == 'no':
-        print('Thank you, have a nice day.')
+        print('Thank you, have a nice day mate.')
         break
